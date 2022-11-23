9e6c32bc5ec81655de7d623f9f40cffcb7f174bd
Belonit <54427022+Belonit@users.noreply.github.com>
2022-11-23 19:51:15 +0300

    Updated project configuration

---
+++ scripts/configure_vscode_cpp.bat
@@ -0,0 +1,14 @@
+@if not defined _echo echo off
+
+rem Copies and opens example config files for VSCode.
+
+rem Ensure we're in correct directory.
+cd /D "%~dp0"
+cd ..\.vscode\
+
+if exist c_cpp_properties.json (
+    echo C++ extension configuration file already present, skipping.
+) else (
+    copy c_cpp_properties.example.json c_cpp_properties.json
+    code c_cpp_properties.json
+)
