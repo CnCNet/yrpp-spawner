4b51091a3026d2c52c48a563e9cc16a41d62f178
Belonit <54427022+Belonit@users.noreply.github.com>
2024-01-21 13:02:08 +0300

    Update Observers.Visibility.cpp
    Replace detached allies check with mutual

--- src/Misc/Observers.Visibility.cpp
+++ src/Misc/Observers.Visibility.cpp
@@ -38,7 +38,7 @@ DEFINE_HOOK(0x45455B, BuildingClass_VisualCharacter_CloakVisibility, 0x5)
 		: CheckMutualAlliance;
 }
 
-// Allow observers and detached allies coordthing cloaked Technos
+// Allow observers and mutual allies coordthing cloaked Technos
 DEFINE_HOOK(0x692540, ScrollClass_Coordthing_TechnoClass_Cloak, 0x5)
 {
 	enum { AllowCoordthing = 0x69256B, CheckSensedByHouses = 0 };
@@ -49,13 +49,13 @@ DEFINE_HOOK(0x692540, ScrollClass_Coordthing_TechnoClass_Cloak, 0x5)
 	GET(TechnoClass*, pTechno, ESI);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return AllowCoordthing;
 
 	return CheckSensedByHouses;
 }
 
-// Allow observers and detached allies coordthing cloaked Buildings
+// Allow observers and mutual allies coordthing cloaked Buildings
 DEFINE_HOOK(0x6925AA, ScrollClass_Coordthing_BuildingClass_Cloak, 0x6)
 {
 	enum { AllowCoordthing = 0x6925F0, CheckSensedByHouses = 0 };
@@ -66,7 +66,7 @@ DEFINE_HOOK(0x6925AA, ScrollClass_Coordthing_BuildingClass_Cloak, 0x6)
 	GET(TechnoClass*, pTechno, ESI);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return AllowCoordthing;
 
 	return CheckSensedByHouses;
@@ -82,13 +82,13 @@ DEFINE_HOOK(0x6DA412, Tactical_SelectAt_Cloak, 0x6)
 	GET(TechnoClass*, pTechno, EAX);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return AllowSelect;
 
 	return CheckSensedByHouses;
 }
 
-// Allow observers and detached allies selected cloaked Technos
+// Allow observers and mutual allies selected cloaked Technos
 DEFINE_HOOK(0x6F4F19, TechnoClass_6F4EB0_Cloak, 0x6)
 {
 	enum { DontUnselect = 0x6F4F3A, CheckSensedByHouses = 0x6F4F21 };
@@ -99,13 +99,13 @@ DEFINE_HOOK(0x6F4F19, TechnoClass_6F4EB0_Cloak, 0x6)
 	GET(TechnoClass*, pTechno, ESI);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return DontUnselect;
 
 	return CheckSensedByHouses;
 }
 
-// Allow observers and detached allies selected cloaked Technos
+// Allow observers and mutual allies selected cloaked Technos
 DEFINE_HOOK(0x4ABE3C, DisplayClass_MouseLeftRelease_Cloak, 0xA)
 {
 	enum { AllowSelect = 0x4ABE4A, Unselect = 0x4ABE88 };
@@ -116,7 +116,7 @@ DEFINE_HOOK(0x4ABE3C, DisplayClass_MouseLeftRelease_Cloak, 0xA)
 	GET(TechnoClass*, pTechno, ESI);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return AllowSelect;
 
 	if (pTechno->IsSensorVisibleToPlayer())
@@ -125,7 +125,7 @@ DEFINE_HOOK(0x4ABE3C, DisplayClass_MouseLeftRelease_Cloak, 0xA)
 	return Unselect;
 }
 
-// Show cloaked Technos on radar for observers and detached allies
+// Show cloaked Technos on radar for observers and mutual allies
 DEFINE_HOOK(0x70D386, TechnoClass_Radar_Cloak, 0xA)
 {
 	enum { Show = 0x70D3CD, DontShow = 0x70D407 };
@@ -136,7 +136,7 @@ DEFINE_HOOK(0x70D386, TechnoClass_Radar_Cloak, 0xA)
 	GET(TechnoClass*, pTechno, ESI);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return Show;
 
 	if (pTechno->IsSensorVisibleToPlayer())
@@ -145,7 +145,7 @@ DEFINE_HOOK(0x70D386, TechnoClass_Radar_Cloak, 0xA)
 	return DontShow;
 }
 
-// Show tooltips for observers and detached allies
+// Show tooltips for observers and mutual allies
 DEFINE_HOOK(0x4AE62B, DisplayClass_HelpText_Cloak, 0x5)
 {
 	enum { CheckIsInvisible = 0x4AE654, CheckSensedByHouses = 0 };
@@ -156,7 +156,7 @@ DEFINE_HOOK(0x4AE62B, DisplayClass_HelpText_Cloak, 0x5)
 	GET(TechnoClass*, pTechno, ECX);
 	const auto pTechnoOwner = pTechno->Owner;
 
-	if (pTechnoOwner && pTechnoOwner->IsAlliedWith(HouseClass::CurrentPlayer))
+	if (pTechnoOwner && pTechnoOwner->IsMutualAllie(HouseClass::CurrentPlayer))
 		return CheckIsInvisible;
 
 	if (pTechno->IsSensorVisibleToPlayer())
