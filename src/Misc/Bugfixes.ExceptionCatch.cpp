fcf0b83a3ea322500a5e35c60bb6158c056962ee
Belonit <54427022+Belonit@users.noreply.github.com>
2024-03-02 08:38:25 +0300

    Minor update Bugfixes.ExceptionCatch.cpp again

--- src/Misc/Bugfixes.ExceptionCatch.cpp
+++ src/Misc/Bugfixes.ExceptionCatch.cpp
@@ -19,41 +19,43 @@
 
 #include <Utilities/Macro.h>
 
-#pragma warning(push)
-#pragma warning(disable: 4702)
-
-LONG __fastcall PrintException(int exception_id, _EXCEPTION_POINTERS* ExceptionInfo)
-{
-	JMP_STD(0x4C8FE0);
-}
-
-LONG __fastcall TopLevelExceptionFilter(int exception_id, _EXCEPTION_POINTERS* ExceptionInfo)
+int TopLevelExceptionFilter(EXCEPTION_POINTERS* ExceptionInfo)
 {
 	DWORD* eip = &(ExceptionInfo->ContextRecord->Eip);
 	switch (*eip)
 	{
-	case 0x7BC806:
-		*eip = 0x7BC80F;
-		return EXCEPTION_CONTINUE_EXECUTION;
-
 	case 0x5D6C21:
+	{
 		// This bug most likely happens when a map Doesn't have Waypoint 90
 		*eip = 0x5D6C36;
 		return EXCEPTION_CONTINUE_EXECUTION;
+	}
 
 	case 0x7BAEA1:
+	{
 		// A common crash in DSurface::GetPixel
 		*eip = 0x7BAEA8;
 		ExceptionInfo->ContextRecord->Ebx = 0;
 		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+
+	case 0x7BC806:
+	{
+		// Something to do with BlitPlain
+		*eip = 0x7BC80F;
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
 
 	case 0x535DBC:
+	{
 		// Common crash in keyboard command class
 		*eip = 0x535DCE;
 		ExceptionInfo->ContextRecord->Esp += 12;
 		return EXCEPTION_CONTINUE_EXECUTION;
+	}
 
 	case 0x000000:
+	{
 		if (ExceptionInfo->ContextRecord->Esp && *(DWORD*)ExceptionInfo->ContextRecord->Esp == 0x55E018)
 		{
 			// A common crash that seems to happen when yuri prime mind controls a building
@@ -62,14 +64,23 @@ LONG __fastcall TopLevelExceptionFilter(int exception_id, _EXCEPTION_POINTERS* E
 			ExceptionInfo->ContextRecord->Esp += 8;
 			return EXCEPTION_CONTINUE_EXECUTION;
 		}
-		return PrintException(exception_id, ExceptionInfo);
-	default:
-		return PrintException(exception_id, ExceptionInfo);
+
+		return EXCEPTION_EXECUTE_HANDLER;
 	}
-	return 0;
+	}
+
+	return EXCEPTION_EXECUTE_HANDLER;
 }
 
+DEFINE_HOOK(0x6BE068, TopLevelExceptionHandler, 0x7)
+{
+	GET_BASE(EXCEPTION_POINTERS*, pExceptionInfo, -0x14);
 
-DEFINE_JUMP(LJMP, 0x6BE06F, GET_OFFSET(TopLevelExceptionFilter))
+	if (TopLevelExceptionFilter(pExceptionInfo) == EXCEPTION_CONTINUE_EXECUTION)
+	{
+		R->EAX(EXCEPTION_CONTINUE_EXECUTION);
+		return 0x6BE074;
+	}
 
-#pragma warning(pop)
+	return 0;
+}
