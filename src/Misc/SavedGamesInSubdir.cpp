0fdde39b223a1d493269bbc61e8167f0ba9a4161
Starkku <starkku.ppmod@gmail.com>
2025-05-28 17:10:07 +0300

    Fixup

--- src/Misc/SavedGamesInSubdir.cpp
+++ src/Misc/SavedGamesInSubdir.cpp
@@ -172,7 +172,7 @@ namespace SavedGames
 {
 	struct CustomMissionID
 	{
-		static constexpr wchar_t* SaveName = L"CustomMissionID";
+		static constexpr const wchar_t* SaveName = L"CustomMissionID";
 
 		int Number;
 
@@ -187,14 +187,14 @@ namespace SavedGames
 
 	struct ExtraTestInfo
 	{
-		static constexpr wchar_t* SaveName = L"Spawner test info";
+		static constexpr const wchar_t* SaveName = L"Spawner test info";
 
 		int CurrentFrame;
 		int TechnoCount;
 
 		explicit ExtraTestInfo()
 			:CurrentFrame { Unsorted::CurrentFrame }
-			, TechnoCount { TechnoClass::Array->Count }
+			, TechnoCount { TechnoClass::Array.Count }
 		{
 		}
 	};
